aa58d4cda09a90b16bef48f5aeb024ebc422039e
abrahaam39 <67434038+abrahaam39@users.noreply.github.com>
2020-07-10 10:25:42 +0200

    h

--- src/test/java/com/barhoum/hello/HelloApplicationTests.java
+++ src/test/java/com/barhoum/hello/HelloApplicationTests.java
@@ -1,10 +1,7 @@
-package com.barhoum.hello;
+package com.barhoum.Hello;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HelloApplicationTests {
+class Hello {
 
 	@Test
 	void contextLoads() {
